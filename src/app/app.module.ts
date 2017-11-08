82c244beeba5aa7fe1a1496db9a870fe9d26ccad
Laptop Nimatuj <1995coral@hotmail.es>
2017-11-07 20:00:57 -0600

    maquetado home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { TitleComponent } from './components/shared/title/title.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TitleComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
